5b3ab69775fc6fab3988440459d6e4ab070ad977
master <augustulus@freenet.de>
2010-01-29 20:00:02 +0100

    linemove, highlevel,strlen removed

--- rest.s
+++ rest.s
@@ -98,26 +98,6 @@ defword "PRESSKEY" , PRESSKEY ,0
             dd EXIT		; EXIT		(return from FORTH word)	; function: ZEIL   TESTED_OK
 
 
-; ; function: STRLEN only tesing
-defword "STRLEN " , STRLEN  ,0
-;: STRLEN 	( str -- len )
-;	DUP		( save start address )
-;	BEGIN
-;		DUP C@ 0<>	( zero byte found? )
-;	WHILE
-;		1+
-;	REPEAT
-
-;	SWAP -		( calculate the length )
-dd DUP
-begin
-dd DUP ,FETCHBYTE,ZNEQU
-while
-dd INCR 
-repeat
-dd SWAP ,SUB
-dd EXIT		; EXIT		(return from FORTH word)
-
 
 
 ; function: WELCOME must be the LAST WORD !! LATEST points here <==
@@ -152,84 +132,5 @@ defword "WELCOM", WELCOM ,0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; no defcode or defword afther this line only for testing
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; function: linemove
-; moves the 'string' to zeile_buffer the line input buffer	
-defword "linemove", linemove ,0
-	LITN text_buffer   ;   destination address
-	LITN 300			;   number of bytes to move
-	dd CMOVE			; memory_move 
-	dd inter    
-	LITN text_buffer
-	dd PPTR , STORE
-	dd EXIT
-; function: highlevel 
-; define some words in pseudo line input compiling. Same as typing the words on keyboard
-defword "highlevel" , highlevel ,0
-;At the moment there is no file system, so we have to simulate lineinput
-	dd DECIMAL
-    LITN text_buffer
-	dd PPTR , STORE
-	LITN comp			;   source
-	dd linemove
-	LITN iff			;   source
-	dd linemove
-	LITN thenn			;   source
-	dd linemove
-	LITN elsee			;   source
-	dd linemove
-	LITN beginn			;   source
-	dd linemove
-	LITN untill			;   source
-	dd linemove
-	LITN againn			;   source
-	dd linemove
-	LITN  whilee		;   source
-	dd linemove
-	LITN repeatt		;   source
-	dd linemove
-	LITN casee			;   source
-	dd linemove
-	LITN off			;   source
-	dd linemove
-	LITN endoff			;   source
-	dd linemove
-	LITN endcasee		;   source
-	dd linemove
-	LITN ckk			;   source
-	dd linemove
-	LITN alli			;   source
-	dd linemove
-	LITN saa			;   source
-	dd linemove
-	LITN dotaa			;   source
-	dd linemove
-	LITN remm			;   source
-	dd linemove
-	dd EXIT
-
-comp: db ': [COMPILE] IMMEDIATE TEILWORT FIND >CFA , ; ',0x0d,0	
-iff: db ': IF IMMEDIATE ', 0x27,' 0BRANCH , HERE @ 0 , ;',0x0d,0
-thenn: db ': THEN IMMEDIATE DUP HERE @ SWAP - SWAP ! ;' , 0x0d,0	
-elsee: db ': ELSE IMMEDIATE ',0x27,' BRANCH , HERE @ 0 , SWAP DUP HERE @ SWAP - SWAP ! ; ',0x0d,0	
-beginn: db ': BEGIN IMMEDIATE HERE @ ; ' , 0x0d,0
-untill: db ': UNTIL IMMEDIATE ',0x27,' 0BRANCH , HERE @ - , ; ' , 0x0d,0
-againn: db ': AGAIN IMMEDIATE ',0x27,' BRANCH , HERE @ - , ; ' , 0x0d,0
-whilee: db ': WHILE IMMEDIATE ',0x27,' 0BRANCH , HERE @ 0 , ; ' , 0x0d,0
-repeatt: db ': REPEAT IMMEDIATE ',0x27,' BRANCH , SWAP HERE @ - , DUP HERE @ SWAP - SWAP ! ; ' , 0x0d,0
-casee: db ': CASE IMMEDIATE 0 ; ', 0x0d,0
-off: db ': OF IMMEDIATE ',0x27,' OVER , ',0x27,' = , [COMPILE] IF ',0x27,' DROP , ; ', 0x0d,0
-endoff: db ': ENDOF IMMEDIATE [COMPILE] ELSE ; ', 0x0d,0
-endcasee: db ': ENDCASE IMMEDIATE ',0x27,' DROP , BEGIN ?DUP WHILE [COMPILE] THEN REPEAT ; ', 0x0d,0
-ckk: db ': C, HERE @ C! 1 HERE +! ; ', 0x0d,0
-alli: db ': ALIGN HERE @ ALIGNED HERE ! ; ', 0x0d,0
-; S" and ." nocht nicht richtig
-saa: db ': S" IMMEDIATE STATE @ .S IF 55 EMIT ',0x27,' LITSTRING , HERE @ 0 , BEGIN KEY1 DUP 22 <> WHILE C, REPEAT DROP DUP HERE @ SWAP - SWAP ! ALIGN ELSE 66 EMIT HERE @ BEGIN KEY1 DUP 22 <> WHILE OVER C! 1+ REPEAT DROP HERE @ - HERE @ SWAP THEN ; ', 0x0d,0
-dotaa: db ': ." IMMEDIATE STATE @ IF [COMPILE] S" ',0x27,' TELL ,  ELSE BEGIN KEY1 DUP 22 <> WHILE EMIT REPEAT THEN ; ', 0x0d,0
-remm: db ': ( DECIMAL IMMEDIATE 1 BEGIN KEY1 DUP 40 = IF DROP 1+ ELSE 41 = IF 1- THEN THEN DUP 0= UNTIL DROP ; ', 0x0d,0
 
 
